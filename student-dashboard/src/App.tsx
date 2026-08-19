b8c772526b9a3ce2f8b5876c1dfb7b704d0f1831
Taneem69 <tanvirtaneem32@gmail.com>
2026-08-19 15:05:34 +0600

    made student card component

---
+++ student-dashboard/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export type Student={
+  id: string,
+  name: string,
+  major: string,
+  gpa: string,
+  avatar: string
+}
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ student-dashboard/src/components/StudentCard.tsx
@@ -0,0 +1,19 @@
+import type {Student} from "../App"
+
+export default function StudentCard(props:{student: Student}){
+    const{student}=props;
+    const gpaColor = Number(student.gpa) >= 3.5 ? "var(--color-gpa-high)" : "#e74c3c";
+
+    return (
+        <div className="student-card">
+            <img src={student.avatar} alt="student.name" className="avatar"/>
+
+            <h3 className="student-name">{student.name}</h3>
+
+            <p className="student-id">ID: {student.id}</p>
+            <p className="student-major">Major: {student.major}</p>
+            <p className="student-gpa" style={{backgroundColor: gpaColor}}>GPA: {student.gpa}</p>
+            
+        </div>
+    )
+}
