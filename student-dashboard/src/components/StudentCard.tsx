cd8bb3467b9b68795285c371cb6ce7d449444f41
Taneem69 <tanvirtaneem32@gmail.com>
2026-08-21 01:02:44 +0600

    updated student card

--- student-dashboard/src/components/StudentCard.tsx
+++ student-dashboard/src/components/StudentCard.tsx
@@ -1,35 +1,40 @@
-import type {Student} from "../App"
+import { useState } from "react";
+import type { Student } from "../App";
 import CourseTag from "./CourseTag";
 import StatBadge from "./StatBadge";
 import "./StudentCard.css";
 
-export default function StudentCard(props:{student: Student}){
-    const{student}=props;
-    const gpaColor = student.gpa >= 3.5 ? "var(--color-gpa-high)" : "#e74c3c";
+function StudentCard(props: {student: Student; onFavoriteToggle: (studentId: string) => void; isFavorite: boolean;}) {
+  const { student, onFavoriteToggle, isFavorite } = props;
 
-    return (
-        <div className="student-card">
-            <img src={student.avatar} alt="student.name" className="avatar"/>
+ 
+  const gpaColor = student.gpa >= 3.5 ? "var(--color-gpa-high)" : "var(--color-gpa-low)";
 
-            <h3 className="student-name">{student.name}</h3>
+  return (
+    <div className="student-card">
+      <div className="card-header">
+        <img src={student.avatar} alt={student.name} className="avatar" />
+        <button className={`favorite-btn ${isFavorite ? "favorite" : ""}`} onClick={() => onFavoriteToggle(student.id)}>
+          {isFavorite ? "❤️" : "🤍"}
+        </button>
+      </div>
+      
+      <h3 className="student-name">{student.name}</h3>
+      <p className="student-id">ID: {student.id}</p>
+      <p className="student-major">Major: {student.major}</p>
+      
+      <div className="courses-container">
+        {student.courses.map((course, index) => (
+          <CourseTag key={index} courseName={course} color={student.courseColors[index]} />
+        ))}
+      </div>
 
-            <p className="student-id">ID: {student.id}</p>
-            <p className="student-major">Major: {student.major}</p>
-
-            <div className="courses-container">
-                {student.courses.map((course, index)=>
-                    <CourseTag key={index} courseName={course} color={student.courseColors[index]}/>
-                )}
-            </div>
-
-
-            <div className="stats-container">
-                <StatBadge label="GPA" value={student.gpa.toString()} color={gpaColor}/>
-
-                <StatBadge label="Credits" value={student.gpa.toString()}/>
-
-            </div>
-            
-        </div>
-    )
+      <div className="stats-container">
+        <StatBadge label="GPA" value={student.gpa.toString()} color={gpaColor} />
+        <StatBadge label="Credits" value={student.credits.toString()} />
+      </div>
+    </div>
+  );
 }
+
+export default StudentCard;
