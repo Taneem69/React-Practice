7082da932961a11584200508617677f2483118b0
Taneem69 <tanvirtaneem32@gmail.com>
2026-08-26 01:21:26 +0600

    modified main.tsx file

--- student-dashboard/src/main.tsx
+++ student-dashboard/src/main.tsx
@@ -1,4 +1,8 @@
 import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import { StudentProvider } from './contexts/StudentContext.tsx';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
